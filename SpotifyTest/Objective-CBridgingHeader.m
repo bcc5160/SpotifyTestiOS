59b933b123f799ac98e2f63753e453dc5e909016
Brittany Chiu <bcc5160@psu.edu>
2016-09-25 20:16:07 -0400

    added bridging header

---
+++ SpotifyTest/Objective-CBridgingHeader.m
@@ -0,0 +1,10 @@
+//
+//  Objective-CBridgingHeader.m
+//  SpotifyTest
+//
+//  Created by Brittany Chiu on 9/25/16.
+//  Copyright © 2016 Brittany Chiu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Spotify/Spotify.h>
